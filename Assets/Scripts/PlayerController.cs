09a6a0c325e11ece50954f050b3f5d6eef9b34ab
Maxime Capel <83071634+fortyup@users.noreply.github.com>
2025-09-16 15:45:22 +0200

    Set the win and lose conditions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,46 +18,59 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent <Rigidbody>();
+        rb = GetComponent<Rigidbody>();
         count = 0;
         totalCount = 12;
 
         SetCountText();
         winTextObject.SetActive(false);
     }
-    void OnMove (InputValue movementValue)
+    void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
 
-        movementX = movementVector.x; 
-        movementY = movementVector.y; 
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 
-    void SetCountText() 
+    void SetCountText()
     {
-        countText.text =  "Count: " + count.ToString();
+        countText.text = "Count: " + count.ToString();
         if (count >= totalCount)
         {
             winTextObject.SetActive(true);
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
         }
     }
 
-    private void FixedUpdate() 
+    private void FixedUpdate()
     {
-        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
-        
-        rb.AddForce(movement * speed); 
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
     }
 
-    void OnTriggerEnter(Collider other) 
+    void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("PickUp")) 
-       {
-        other.gameObject.SetActive(false);
-        count = count + 1;
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
 
-        SetCountText();
-       }
+            SetCountText();
+        }
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+    if (collision.gameObject.CompareTag("Enemy"))
+    {
+        // Destroy the current object
+        Destroy(gameObject); 
+        // Update the winText to display "You Lose!"
+        winTextObject.gameObject.SetActive(true);
+        winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+    }
     }
 
 }
